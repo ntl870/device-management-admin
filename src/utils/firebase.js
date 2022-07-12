815f16225cf0752111e289dd3a70a97765f44725
Long Nguyen <ntlong870@gmail.com>
2022-07-13 00:42:11 +0700

    firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyA8dydc9XF6TJaAYTTX9MNdyin5dTYQiNU",
-  authDomain: "book-management-admin.firebaseapp.com",
-  projectId: "book-management-admin",
-  storageBucket: "book-management-admin.appspot.com",
-  messagingSenderId: "766238808224",
-  appId: "1:766238808224:web:989991877b80a8c38911c5",
+  apiKey: "AIzaSyDzPt47358FEtBw-SpVKNhzc5eGvrpnWSk",
+  authDomain: "device-management-admin.firebaseapp.com",
+  projectId: "device-management-admin",
+  storageBucket: "device-management-admin.appspot.com",
+  messagingSenderId: "173978718347",
+  appId: "1:173978718347:web:00be816a34f951dcc1448e",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
